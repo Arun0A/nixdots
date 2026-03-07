43090056f4eabc4b3e1b9e0caf5f6aca44e1bf54
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-03-08 00:17:29 +0530

    add TLP battery saving program

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,25 @@
   # networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
   ################
+  # Battery
+  ################
+  # Disable if devices take long to unsuspend (keyboard, mouse, etc)
+  powerManagement.powertop.enable = true;
+  services = {
+    power-profiles-daemon.enable = false;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        STOP_CHARGE_THRESH_BAT1 = 95;
+      };
+    };
+  };
+
+  ################
   # Time
   ################
 
