2c8abbd6625b78bf6e1d2883bcd4b0693d4291bd
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-03-08 12:45:46 +0530

    add and config logiops

--- configuration.nix
+++ configuration.nix
@@ -186,6 +186,7 @@
 
     bluez
     blueman
+    logiops
 
     cloudflare-warp
     evtest
@@ -209,6 +210,82 @@
   programs.slock.enable = true;
 
   programs.zsh.enable = true;
+
+  ################
+  # logiops
+  ################
+  # Create systemd service
+  systemd.services.logiops = {
+    description = "An unofficial userspace driver for HID++ Logitech devices";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.logiops}/bin/logid";
+    };
+  };
+
+  # Configuration for logiops
+  environment.etc."logid.cfg".text = ''
+    devices: ({
+      name: "Wireless Mouse MX Anywhere 3S";
+      smartshift: {
+        on: false;
+        threshold: 12;
+      };
+      hiresscroll: {
+        hires: true;
+        target: false;
+      };
+      dpi: 1200;
+      buttons: ({
+        cid: 0xc3;
+        action = {
+          type: "Gestures";
+          gestures: ({
+            direction: "Left";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_F15"];
+            };
+          }, {
+            direction: "Right";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_F16"];
+            };
+          }, {
+            direction: "Down";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: ["KEY_F17"];
+            };
+          }, {
+            direction: "Up";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: ["KEY_F18"];
+            };
+          }, {
+            direction: "None";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_PLAYPAUSE"];
+            };
+          });
+        };
+      }, {
+        cid: 0xc4;
+        action = {
+          type: "Keypress";
+          keys: ["KEY_F19"];
+        };
+      });
+    });
+  '';
 
   ################
   # Nix settings
