d99bf10521f602d5ae0d5844f76174b953256e8a
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-03-08 14:12:53 +0530

    improve dwm slock locking behaviour

--- dwm/config.h
+++ dwm/config.h
@@ -110,7 +110,7 @@ static const Key keys[] = {
 	{ SUPKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ SUPKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ SUPKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ SUPKEY|ShiftMask,             XK_l,      spawn,          SHCMD("slock & sleep 0.1 && xset dpms force off") },
+	{ SUPKEY|ShiftMask,             XK_l,      spawn,          SHCMD("xset dpms force off; slock") },
 	{ 0,			XF86XK_MonBrightnessUp,    spawn,          {.v = brupcmd}},
 	{ 0,			XF86XK_MonBrightnessDown,    spawn,          {.v = brdncmd}},
 	{ 0, 			XF86XK_AudioLowerVolume,   spawn, 	SHCMD("pamixer -d 5 --allow-boost; ~/nix-dots/dwm/vol_update.sh") },
