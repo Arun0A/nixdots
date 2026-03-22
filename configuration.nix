4d7f64d876e4e0b6c76ad61a5797f395c4addc44
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-03-23 00:04:30 +0530

    enable ipv6 forwarding: sysctl

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,11 @@ in
 
   boot.kernelModules = [ "coretemp" ];
 
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = true;
+    "net.ipv6.conf.all.forwarding" = true;
+  };
+
   ################
   # Networking
   ################
