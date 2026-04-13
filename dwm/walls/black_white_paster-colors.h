fc4bc50272ed2ac3b5ce6b5ba9d5150be1f472a9
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-04-13 12:01:04 +0530

    dwm: dynamic color scheme setup

---
+++ dwm/walls/black_white_paster-colors.h
@@ -0,0 +1,16 @@
+// color scheme for green_void.png 
+static const char col_gray1[]       = "#222222"; /* Mine Shaft */
+static const char col_gray2[]       = "#444444"; /* Tundora */ 
+static const char col_gray3[]       = "#bbbbbb"; /* Silver */
+static const char col_gray4[]       = "#eeeeee"; /* Gallery */
+static const char col_cyan[]        = "#005577"; /* Orient */
+
+static const char col_mingreen[]    = "#414141"; /* Mineral Green */
+static const char col_shark[]       = "#232424"; /* Shark */
+
+static const char *colors[][3]      = {
+	/*               fg            bg            border       */
+	[SchemeNorm] = { col_gray3,    col_shark, 	 col_shark     },
+	[SchemeSel]  = { col_gray4,    col_mingreen, col_mingreen  },
+	[SchemeHid]  = { col_mingreen, col_shark,    col_mingreen  },
+};
---
+++ dwm/walls/renault_rs19-colors.h
@@ -0,0 +1,18 @@
+// color scheme for green_void.png 
+static const char col_gray1[]       = "#222222"; /* Mine Shaft */
+static const char col_gray2[]       = "#444444"; /* Tundora */ 
+static const char col_gray4[]       = "#090907"; /* Silver */
+static const char col_gray3[]       = "#2a2925"; /* Gallery */
+static const char col_cyan[]        = "#005577"; /* Orient */
+
+static const char col_hiddenfg[]    = "#4c3c0b"; /* brown */
+static const char col_mingreen[]    = "#7a6702"; /* dark yellow */
+static const char col_textfg[]      = "#f3ec36"; /* dark yellow */
+static const char col_shark[]       = "#232424"; /* Shark */
+
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_textfg, col_shark, 	   col_shark     },
+	[SchemeSel]  = { col_shark, col_mingreen,  col_mingreen  },
+	[SchemeHid]  = { col_hiddenfg, col_shark,  col_mingreen  },
+};
