2b09ce8c3848fd689fea94f3cd27ff501e4ae67c
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-04-17 00:29:02 +0530

    add separate v4l2 devices

--- configuration.nix
+++ configuration.nix
@@ -49,10 +49,9 @@ in
   ];
 
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 \
-      card_label="OBS Cam" \
-      exclusive_caps=1 \
-      max_buffers=2
+    options v4l2loopback devices=2 video_nr=1,2 \
+      card_label="DroidCam","OBS Cam" \
+      exclusive_caps=1
   '';
 
   boot.kernel.sysctl = {
