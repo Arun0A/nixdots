50b54dea13af6700779b645db0a1d509290ba7e7
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-04-28 15:24:45 +0530

    fix: Launch6 key not working after del remap

--- hosts/yoga14.nix
+++ hosts/yoga14.nix
@@ -7,11 +7,11 @@
     evdev:input:b0019v0000p0000e0000-e0,1,4,k89,8A,94,95,BA,CA,CB,D4,E3,F0,F7,F8,16C,1BD,1BE,212,213,214,232,26A,27A,ram4,lsfw
       KEYBOARD_KEY_101=f13
       KEYBOARD_KEY_10e=f14
-      KEYBOARD_KEY_10f=delete
+      KEYBOARD_KEY_10f=f15
 
     evdev:name:Ideapad extra buttons:dmi:bvn*:bvr*:bd*:svnLENOVO*:pn21DM*:*
       KEYBOARD_KEY_101=f13
       KEYBOARD_KEY_10e=f14
-      KEYBOARD_KEY_10f=delete
+      KEYBOARD_KEY_10f=f15
   '';
 }
