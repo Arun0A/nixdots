3f77ceb4cc84c9ba2f3eb4e650999ded1ade2bf4
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-05-25 12:38:19 +0530

    tmux new win config

--- yoga14-pegion-home.nix
+++ yoga14-pegion-home.nix
@@ -257,6 +257,7 @@ in
       set-option -g allow-rename off
       bind '"' split-window -v -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
+	  bind c new-window -c "#{pane_current_path}"
     '';
   };
 
