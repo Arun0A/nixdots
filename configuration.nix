b69bd0434ee138ff23ab8a1a7d30f285ab54256c
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-05-30 16:51:39 +0530

    fix pulseaudio

--- configuration.nix
+++ configuration.nix
@@ -161,11 +161,12 @@ in
   # Pipewire
   ################
 
+  services.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
-
+	alsa.support32Bit = true;
     wireplumber.enable = true;
   };
 
