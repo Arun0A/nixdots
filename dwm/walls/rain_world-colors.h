b91ea800b801f041dbd8c00b7c6bddcb18358d86
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-06-06 23:06:54 +0530

    new rain_world color theme

---
+++ dwm/walls/rain_world-colors.h
@@ -0,0 +1,15 @@
+// color scheme for rain_world.png 
+static const char col_gray1[]       = "#222222"; /* Mine Shaft */
+static const char col_gray2[]       = "#444444"; /* Tundora */ 
+static const char col_gray3[]       = "#d99f7e"; /* Silver */
+static const char col_gray4[]       = "#829c84"; /* Gallery */
+static const char col_cyan[]        = "#005577"; /* Orient */
+static const char col_mingreen[]    = "#cc784e"; /* Mineral Green */
+static const char col_shark[]       = "#2d0723"; /* Shark */
+
+static const char *colors[][3]      = {
+	/*               fg            bg            border       */
+	[SchemeNorm] = { col_mingreen,    col_shark, 	 col_shark     },
+	[SchemeSel]  = { col_shark,    col_mingreen, col_mingreen  },
+	[SchemeHid]  = { col_gray4, col_shark,    col_mingreen  },
+};
