42ab79ecd414afa6002e041bb30e5e23b72c3b89
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-06-16 00:12:12 +0530

    disable all fprint

--- fprint55a2.nix
+++ fprint55a2.nix
@@ -1,15 +1,15 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.fprintd.enable = true;
+  services.fprintd.enable = false;
 
   systemd.services.fprintd.environment.LD_LIBRARY_PATH =
     "/opt/fprint55a2/lib";
 
-  security.pam.services.sudo.fprintAuth = true;
+  security.pam.services.sudo.fprintAuth = false;
 
   security.pam.services.slock = {};
-  security.pam.services.slock.fprintAuth = true;
+  security.pam.services.slock.fprintAuth = false;
 
   security.polkit.extraConfig = ''
     polkit.addRule(function(action, subject) {
