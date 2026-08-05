2bf759776586023dc47e6f0fdeb7635af2e97195
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-08-06 00:14:46 +0530

    add tmux pane switch binds

--- yoga14-pegion-home.nix
+++ yoga14-pegion-home.nix
@@ -436,6 +436,16 @@ in
       # unbind '"'
       # unbind %
 
+	  bind-key -n M-1 select-window -t :1
+	  bind-key -n M-2 select-window -t :2
+	  bind-key -n M-3 select-window -t :3
+	  bind-key -n M-4 select-window -t :4
+	  bind-key -n M-5 select-window -t :5
+	  bind-key -n M-6 select-window -t :6
+	  bind-key -n M-7 select-window -t :7
+	  bind-key -n M-8 select-window -t :8
+	  bind-key -n M-9 select-window -t :9
+
       bind -n M-J select-pane -D
       bind -n M-K select-pane -U
       bind -n M-H select-pane -R
