193099b54cf0c1cd5c9d07c8c82c89f6272b9292
Arun Bhattacharya <arun0a_in@hotmail.com>
2026-08-21 00:03:00 +0530

    another wall, another theme

---
+++ dwm/walls/junji-ito-2-colors.h
@@ -0,0 +1,15 @@
+// color scheme for fractal.png 
+static const char col_gray1[]       = "#eddfc4"; /* Mine Shaft */
+static const char col_gray2[]       = "#4f331b"; /* Tundora */ 
+static const char col_gray3[]       = "#242a40"; /* Silver */
+static const char col_gray4[]       = "#49515c"; /* Gallery */
+static const char col_cyan[]        = "#005577"; /* Orient */
+static const char col_mingreen[]    = "#fff9dc"; /* Mineral Green */
+static const char col_shark[]       = "#010217"; /* Shark */
+
+static const char *colors[][3]      = {
+	/*               fg            bg            border       */
+	[SchemeNorm] = { col_gray1,    col_shark, 	 col_shark     },
+	[SchemeSel]  = { col_mingreen,    col_gray3,    col_gray1  },
+	[SchemeHid]  = { col_gray4,    col_shark,    col_gray3  },
+};
